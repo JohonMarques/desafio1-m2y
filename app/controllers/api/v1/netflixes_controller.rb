2cb60f783145438a92918750005b7dcdfec2f0b4
Johon Marques <johonblk@gmail.com>
2021-11-17 20:18:45 -0300

    padronização do rubocop

--- app/controllers/api/v1/netflixes_controller.rb
+++ app/controllers/api/v1/netflixes_controller.rb
@@ -1,60 +1,63 @@
 module Api
-    module V1
-      class NetflixesController < ApplicationController
-        # GET http://localhost:3000/api/v1/netflixes
-        # GET http://localhost:3000/api/v1/netflixes?release_year=2021&country=brazil&genre=movie
+  module V1
+    class NetflixesController < ApplicationController
+      # GET http://localhost:3000/api/v1/netflixes
+      # GET http://localhost:3000/api/v1/netflixes?release_year=2021&country=brazil&genre=movie
 
-        def index
-          movies = Netflix.order(release_year: :asc)
-          movies = Netflix.where("release_year LIKE ? or lower(country) LIKE ? or lower(genre) LIKE ?", params[:release_year],  params[:country], params[:genre]) if params[:release_year] or params[:country] or params[:genre].present?
-          render json: { status: 'SUCCESS', message: 'Movies List', data: movies }, status: :ok
+      def index
+        movies = Netflix.order(release_year: :asc)
+        if params[:release_year] or params[:country] or params[:genre].present?
+          movies = Netflix.where('release_year LIKE ? or lower(country) LIKE ? or lower(genre) LIKE ?',
+                                 params[:release_year], params[:country], params[:genre])
         end
-        
-        # GET http://localhost:3000/api/v1/netflixes/1
-  
-        def show
-          movie = Netflix.find(params[:id])
-          render json: { status: 'SUCCESS', message: 'Movie Founded', data: movie }, status: :ok
-        end
+        render json: { status: 'SUCCESS', message: 'Movies List', data: movies }, status: :ok
+      end
 
-        #POST http://localhost:3000/api/v1/netflixes
-  
-        def create
-          movie = Netflix.new(netflix_params)
-          if movie.save
-            render json: { status: 'SUCCESS', message: 'Movie Added', data: movie }, status: :ok
-          else
-            render json: { status: 'ERROR', message: 'Failed to add movie', data: movie.err },
-                   status: :unprocessable_entity
-          end
-        end
+      # GET http://localhost:3000/api/v1/netflixes/1
 
-        # PUT http://localhost:3000/api/v1/netflixes/1
-  
-        def update
-          movie = Netflix.find(params[:id])
-          if movie.update(netflix_params)
-            render json: { status: 'SUCCESS', message: 'Movie updated', data: movie }, status: :ok
-          else
-            render json: { status: 'ERROR', message: 'Movie can not be updated', data: movie },
-                   status: :unprocessable_entity
-          end
-        end
+      def show
+        movie = Netflix.find(params[:id])
+        render json: { status: 'SUCCESS', message: 'Movie Founded', data: movie }, status: :ok
+      end
 
-        # DELETE http://localhost:3000/api/v1/netflixes/1
+      # POST http://localhost:3000/api/v1/netflixes
 
-        def destroy
-          movie = Netflix.find(params[:id])
-          movie.destroy
-          render json: { status: 'SUCCESS', message: 'Movie deleted.', data: movie }, status: :ok
+      def create
+        movie = Netflix.new(netflix_params)
+        if movie.save
+          render json: { status: 'SUCCESS', message: 'Movie Added', data: movie }, status: :ok
+        else
+          render json: { status: 'ERROR', message: 'Failed to add movie', data: movie.err },
+                 status: :unprocessable_entity
         end
-  
-        private
-  
-        def netflix_params
-          params.permit(:show_id, :genre, :title, :director, :cast, :country, :date_added, :release_year, :rating, :duration, :listed_in, :description)
+      end
+
+      # PUT http://localhost:3000/api/v1/netflixes/1
+
+      def update
+        movie = Netflix.find(params[:id])
+        if movie.update(netflix_params)
+          render json: { status: 'SUCCESS', message: 'Movie updated', data: movie }, status: :ok
+        else
+          render json: { status: 'ERROR', message: 'Movie can not be updated', data: movie },
+                 status: :unprocessable_entity
         end
+      end
+
+      # DELETE http://localhost:3000/api/v1/netflixes/1
+
+      def destroy
+        movie = Netflix.find(params[:id])
+        movie.destroy
+        render json: { status: 'SUCCESS', message: 'Movie deleted.', data: movie }, status: :ok
+      end
+
+      private
+
+      def netflix_params
+        params.permit(:show_id, :genre, :title, :director, :cast, :country, :date_added, :release_year, :rating,
+                      :duration, :listed_in, :description)
       end
     end
   end
-  
+end
--- app/models/netflix.rb
+++ app/models/netflix.rb
@@ -1,6 +1,5 @@
 class Netflix < ApplicationRecord
-    validates :title, presence: true, uniqueness: true
-    validates :release_year, presence: true, length: { is: 4 }
-    validates :duration, :description, :genre, presence: true
-
+  validates :title, presence: true, uniqueness: true
+  validates :release_year, presence: true, length: { is: 4 }
+  validates :duration, :description, :genre, presence: true
 end
